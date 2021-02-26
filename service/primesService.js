a06a2677710461aed09c98cb378b22cf2509abed
José Luis <joseluis.jlc@gmail.com>
2021-02-25 21:52:23 -0300

    prime numbers

--- service/primesService.js
+++ service/primesService.js
@@ -1,9 +1,9 @@
 const validator = require("./parameterValidator");
-const primes = require("./calculatorService");
+const calculator = require("./calculatorService");
 
 const getPrimes = (parameter) => {
   if (validator.validateParameter(parameter)) {
-    return primes.getListPrimesTo(parameter);
+    return calculator.getListPrimesTo(parameter);
   } else {
     throw new Error(
       "The program does not accept numbers other than positive natives."
