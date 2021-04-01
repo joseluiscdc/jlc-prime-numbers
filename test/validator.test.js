e54923bdb3e457766d645419b04e05417f977e58
José Luis <joseluis.jlc@gmail.com>
2021-04-01 01:56:18 -0300

    test jenkins

--- test/validator.test.js
+++ test/validator.test.js
@@ -13,6 +13,10 @@ describe("validate that it only receives positive integers", () => {
     expect(validator.validateParameter("1/7")).toEqual(false);
   });
 
+  it("does not match a fraction 1/9", () => {
+    expect(validator.validateParameter("1/9")).toEqual(false);
+  });
+
   it("match a edge case", () => {
     expect(validator.validateParameter(98989959595959595959595998989)).toEqual(
       true
